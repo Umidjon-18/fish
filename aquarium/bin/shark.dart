168a5a4a4f6ba8140a4d5bbe18e1ecedc9dc725f
Umidjon Yoqubov <umidjonxomidjonovich@gmail.com>
2022-06-08 21:09:23 +0500

    Update shark.dart

--- aquarium/bin/shark.dart
+++ aquarium/bin/shark.dart
@@ -16,17 +16,20 @@ class Shark {
     if (aquariumInterface.getSizeFish() > 20) {
       Fish victim = aquariumInterface.getFish()[killing(aquariumInterface.getFishA(), aquariumInterface.getFishB())];
       victim.onDead("Shark killed 🦈");
-    Timer(Duration(seconds: 3), ()=> checking(aquariumInterface.getSizeFish()));
+    Timer(Duration(seconds: 5), ()=> checking(aquariumInterface.getSizeFish()));
     } else {
-    Timer(Duration(seconds: 10), ()=> checking(aquariumInterface.getSizeFish()));
+    Timer(Duration(seconds: 15), ()=> checking(aquariumInterface.getSizeFish()));
     }
   }
 
   String killing(List<String> listFishA, List<String> listFishB) {
-    if (listFishA.length > listFishB.length) {
+    if (listFishA.length > listFishB.length + 10) {
       return listFishA[random.nextInt(listFishA.length)];
-    } else {
+    } else if(listFishB.length > listFishA.length + 10){
       return listFishB[random.nextInt(listFishB.length)];
+    } else {
+      var allName = [...listFishA, ...listFishB];
+      return allName[random.nextInt(allName.length)];
     }
   }
 
